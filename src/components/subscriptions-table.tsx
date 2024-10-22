428983bb6e2a4922687ccc3f1be3263c29195819
Khalil <khalilpati55@gmail.com>
2024-10-22 23:23:43 +0530

    Add edit button to edit a subscription

--- src/components/subscriptions-table.tsx
+++ src/components/subscriptions-table.tsx
@@ -12,10 +12,13 @@ import {
 import { useSubscription } from "@/store/use-subscription";
 import { Button } from "./ui/button";
 import { Trash } from "@phosphor-icons/react/dist/ssr";
+import { Edit } from "lucide-react";
+import { useModal } from "@/store/use-modal";
 
 const SubscriptionTable = () => {
   const subscriptions = useSubscription((state) => state.subscriptions);
   const remove = useSubscription((state) => state.removeSubscription);
+  const setModal = useModal((state) => state.setModal);
   return (
     <section className="w-full">
       <h2 className="mb-3 text-xl font-bold text-primary-foreground">
@@ -42,12 +45,22 @@ const SubscriptionTable = () => {
                 <TableCell>{subscription.date}</TableCell>
                 <TableCell>${subscription.price}</TableCell>
                 <TableCell>
-                  <Button
-                    size="icon"
-                    variant="destructive"
-                    onClick={() => remove(subscription.id)}>
-                    <Trash />
-                  </Button>
+                  <div className="flex items-center gap-2">
+                    <Button
+                      size="icon"
+                      variant="ghost"
+                      onClick={() =>
+                        setModal({ modal: true, subscription, view: "edit" })
+                      }>
+                      <Edit />
+                    </Button>
+                    <Button
+                      size="icon"
+                      variant="destructive"
+                      onClick={() => remove(subscription.id)}>
+                      <Trash />
+                    </Button>
+                  </div>
                 </TableCell>
               </TableRow>
             ))}
