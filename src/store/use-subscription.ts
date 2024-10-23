04a6803c219e11ea13f041013b58577f72375988
Khalil <khalilpati55@gmail.com>
2024-10-23 15:10:19 +0530

    Persist data using zustand middleware

--- src/store/use-subscription.ts
+++ src/store/use-subscription.ts
@@ -1,6 +1,7 @@
 /* eslint-disable no-unused-vars */
 import { Subscription } from "@/types";
 import { create } from "zustand";
+import { persist } from "zustand/middleware";
 
 type State = {
   subscriptions: Subscription[];
@@ -12,25 +13,30 @@ type Actions = {
   updateSubscription: (id: string, subscription: Partial<Subscription>) => void;
 };
 
-export const useSubscription = create<State & Actions>()((set) => ({
-  subscriptions: [],
-  addSubscription: (subscription) => {
-    set((state) => ({
-      subscriptions: [...state.subscriptions, subscription],
-    }));
-  },
-  removeSubscription: (id) => {
-    set((state) => ({
-      subscriptions: state.subscriptions.filter(
-        (subscription) => subscription.id !== id
-      ),
-    }));
-  },
-  updateSubscription: (id, subscription) => {
-    set((state) => ({
-      subscriptions: state.subscriptions.map((sub) =>
-        sub.id === id ? { ...sub, ...subscription } : sub
-      ),
-    }));
-  },
-}));
+export const useSubscription = create<State & Actions>()(
+  persist(
+    (set) => ({
+      subscriptions: [],
+      addSubscription: (subscription) => {
+        set((state) => ({
+          subscriptions: [...state.subscriptions, subscription],
+        }));
+      },
+      removeSubscription: (id) => {
+        set((state) => ({
+          subscriptions: state.subscriptions.filter(
+            (subscription) => subscription.id !== id
+          ),
+        }));
+      },
+      updateSubscription: (id, subscription) => {
+        set((state) => ({
+          subscriptions: state.subscriptions.map((sub) =>
+            sub.id === id ? { ...sub, ...subscription } : sub
+          ),
+        }));
+      },
+    }),
+    { name: "subscription-store" }
+  )
+);
