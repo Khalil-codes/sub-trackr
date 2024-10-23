793d566c4e09251ffb83cfd7d741145ecaa5002a
Khalil <khalilpati55@gmail.com>
2024-10-24 02:02:52 +0530

    Add meta title and dynamic title based on subscriptions

--- src/components/toal-amount.tsx
+++ src/components/toal-amount.tsx
@@ -1,17 +1,32 @@
 "use client";
 
 import { useSubscription } from "@/store/use-subscription";
-import React from "react";
+import React, { useEffect, useMemo } from "react";
+
+const defaultTitle = "Subscriptions Tracker";
 
 const TotalAmount = () => {
   const subscriptions = useSubscription((state) => state.subscriptions);
 
+  const totalAmount = useMemo(() => {
+    return subscriptions.map((sub) => sub.price).reduce((a, b) => a + b, 0);
+  }, [subscriptions]);
+
+  useEffect(() => {
+    if (document && subscriptions.length > 0) {
+      document.title = `${subscriptions.length} (₹${totalAmount}) Subscriptions`;
+    }
+    return () => {
+      document.title = defaultTitle;
+    };
+  }, [totalAmount, subscriptions]);
+
   return (
-    <h2 className="text-xl font-bold text-primary">
+    <h2 className="font-giest-mono text-xl font-bold text-primary">
       Total Amount:{" "}
-      <span className="text-3xl">
+      <span className="text-2xl">
         ₹&nbsp;
-        {subscriptions.map((sub) => sub.price).reduce((a, b) => a + b, 0)}
+        {totalAmount}
       </span>
     </h2>
   );
