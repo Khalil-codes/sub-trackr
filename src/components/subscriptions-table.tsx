529803aebfdc4cb559a6b7ca215db6f351dd868b
Khalil <khalilpati55@gmail.com>
2024-10-23 15:00:06 +0530

    Styld table component and display proper data

--- src/components/subscriptions-table.tsx
+++ src/components/subscriptions-table.tsx
@@ -11,8 +11,6 @@ import {
 } from "./ui/table";
 import { useSubscription } from "@/store/use-subscription";
 import { Button } from "./ui/button";
-import { Trash } from "@phosphor-icons/react/dist/ssr";
-import { Edit } from "lucide-react";
 import { useModal } from "@/store/use-modal";
 
 const SubscriptionTable = () => {
@@ -24,41 +22,54 @@ const SubscriptionTable = () => {
       <h2 className="mb-3 text-xl font-bold text-primary-foreground">
         Subscriptions
       </h2>
-      <div className="overflow-auto rounded-lg p-4 text-white shadow-sm">
+      <div className="overflow-auto rounded-lg bg-black p-6 text-primary-foreground shadow-sm dark:bg-black">
         <Table>
-          <TableHeader className="uppercase">
-            <TableRow>
-              <TableHead>Subscription</TableHead>
-              <TableHead>Date</TableHead>
-              <TableHead>Cost</TableHead>
-              <TableHead>Actions</TableHead>
+          <TableHeader className="bg-gray-800/50 uppercase [&_tr]:border-none">
+            <TableRow className="rounded-xl hover:bg-gray-900">
+              <TableHead className="h-14 text-primary-foreground">
+                Subscription
+              </TableHead>
+              <TableHead className="h-14 text-primary-foreground">
+                Date
+              </TableHead>
+              <TableHead className="h-14 text-primary-foreground">
+                Cost
+              </TableHead>
+              <TableHead className="h-14 text-primary-foreground">
+                Actions
+              </TableHead>
             </TableRow>
           </TableHeader>
           <TableBody>
             {subscriptions.map((subscription) => (
-              <TableRow key={subscription.id}>
+              <TableRow
+                key={subscription.id}
+                className="border-b-[0.5px] border-b-gray-500 hover:bg-gray-900">
                 <TableCell>
                   <div className="flex items-center gap-2">
-                    <subscription.service.icon /> {subscription.service.name}
+                    <subscription.service.icon size={20} />{" "}
+                    <span>{subscription.service.name}</span>
                   </div>
                 </TableCell>
                 <TableCell>{subscription.date}</TableCell>
-                <TableCell>${subscription.price}</TableCell>
+                <TableCell>₹{subscription.price}</TableCell>
                 <TableCell>
                   <div className="flex items-center gap-2">
                     <Button
-                      size="icon"
-                      variant="ghost"
+                      size="sm"
+                      className="py-1"
+                      variant="secondary"
                       onClick={() =>
                         setModal({ modal: true, subscription, view: "edit" })
                       }>
-                      <Edit />
+                      Edit
                     </Button>
                     <Button
-                      size="icon"
+                      size="sm"
+                      className="py-1"
                       variant="destructive"
                       onClick={() => remove(subscription.id)}>
-                      <Trash />
+                      Delete
                     </Button>
                   </div>
                 </TableCell>
